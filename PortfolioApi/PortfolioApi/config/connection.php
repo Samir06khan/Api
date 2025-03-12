4e6fb26666d5805a0b46d26e14c69ee6b2f32ae1
Samir Khan <120166120+Samir06khan@users.noreply.github.com>
2025-03-13 01:50:42 +0530

    connection.php

--- PortfolioApi/PortfolioApi/config/connection.php
+++ PortfolioApi/PortfolioApi/config/connection.php
@@ -1,3 +1,8 @@
-<?php 
+<?php
 
-$conn = mysqli_connect("localhost","root","1234","portfolio_db") or die("Connection Failed" . mysqli_connect_error());
+$conn = mysqli_connect(
+    "localhost",
+    "root",
+    "1234",
+    "portfolio_db"
+) or die("Connection Failed" . mysqli_connect_error());
